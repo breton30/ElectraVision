4be659556cf93a2215e154dcaaa9789066d9954d
Iván Borraz García <ivanborrazgarcia@gmail.com>
2024-02-27 10:55:30 +0100

    Pila acabado

--- Pila.java
+++ Pila.java
@@ -1,10 +1,22 @@
 public class Pila{
     private int horasDeVida;
     private String tipoPila;
+    public Pila () {
+        horasDeVida = 0;
+        tipoPila = "Duracell";
+    }
     public int getHorasDeVida(){
         return horasDeVida;
     }
     public void setTipoPila(String tipoDePila){
         tipoPila = tipoDePila;
+    }
+    
+    public String getTipoPila () {
+        return tipoPila;
+    }
+    
+    public void setHorasDeVida (int horasUsado) {
+        horasDeVida = horasUsado;
     }
 }
