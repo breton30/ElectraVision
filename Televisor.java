05d9c8c7b865dfa9041245685d0dabfe62114c1d
Ronald <ronaldbresan@gmail.com>
2024-02-27 11:18:00 +0100

    tercer commit

---
+++ Televisor.java
@@ -0,0 +1,11 @@
+
+public class Televisor{
+    private int canales;
+    private boolean encendido;
+    private Mando mando;
+    public Televisor(int canales, boolean encendido, Mando mando){
+        this.canales = canales;
+        this.encendido = encendido;
+        this.mando = mando;
+    }
+}
