7c95dead5e20462abd9bad398cd9e770c8d6ec4e
Ronald <ronaldbresan@gmail.com>
2024-02-27 11:02:48 +0100

    Segundo commit

---
+++ Mando.java
@@ -0,0 +1,11 @@
+
+public class Mando{
+    private int horasUsado;
+    private int canales;
+    private Pila pila;
+    public Mando(int horasUsado, int canales, Pila pila){
+        this.horasUsado = horasUsado;
+        this.canales = canales;
+        this.pila = pila;
+    }
+}
