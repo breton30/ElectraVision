02de03c5bc69c74414f61b3960221c979ffb069e
Ronald <ronaldbresan@gmail.com>
2024-02-27 10:47:42 +0100

    primer commit

---
+++ Pila.java
@@ -0,0 +1,10 @@
+public class Pila{
+    private int horasDeVida;
+    private String tipoPila;
+    public int getHorasDeVida(){
+        return horasDeVida;
+    }
+    public void setTipoPila(String tipoDePila){
+        tipoPila = tipoDePila;
+    }
+}
